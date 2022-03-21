18c49aede41ab731f3e1da57ad91c8f73df3f909
kimin3004 <kimin3004@gmail.com>
2022-03-21 22:08:40 +0900

    feat: Network 연결상태에 따른 Notification 추가

--- webpack.main.js
+++ webpack.main.js
@@ -5,6 +5,7 @@ const baseConfig = require('./webpack.base');
 module.exports = merge(baseConfig, {
   entry: {
     main: './src/main/main.ts',
+    preload: './src/main/preload.ts',
   },
   target: 'electron-main',
 });
